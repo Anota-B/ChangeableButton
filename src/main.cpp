e8eb28cfccb581411164430c215eb855f03ff027
Anota-B <abonnot15@gmail.com>
2024-09-02 23:30:50 +0200

    Writing hook function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include "main.hpp"
 
+#include "GlobalNamespace/StandardLevelDetailView.hpp"
+#include "GlobalNamespace/StandardLevelDetailViewController.hpp"
+#include "UnityEngine/UI/Button.hpp"
+#include "UnityEngine/GameObject.hpp"
+#include "HMUI/CurvedTextMeshPro.hpp"
+
+
 #include "scotland2/shared/modloader.h"
 
 static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
@@ -14,6 +21,24 @@ Configuration &getConfig() {
   return config;
 }
 
+MAKE_HOOK_MATCH(LevelUIHook, &GlobalNamespace::StandardLevelDetailViewController::DidActivate, void,
+ GlobalNamespace::StandardLevelDetailViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+    // Run the original method before our code.
+    // Note that you can run the original method after our code or even in the middle
+    // if you want to change arguments or do something before it runs.
+    LevelUIHook(self, firstActivation, addedToHierarchy, screenSystemEnabling);
+
+    // Get the actionButton text object by accessing the actionButton field and some simple Unity methods.
+    // Note that auto can be used instead of declaring the full type in many cases.
+    GlobalNamespace::StandardLevelDetailView* standardLevelDetailView = self->_standardLevelDetailView;
+    UnityEngine::UI::Button* actionButton = standardLevelDetailView->actionButton;
+    UnityEngine::GameObject* gameObject = actionButton->get_gameObject();
+    HMUI::CurvedTextMeshPro* actionButtonText = gameObject->GetComponentInChildren<HMUI::CurvedTextMeshPro*>();
+
+    // Set the text to "Skill Issue"
+    actionButtonText->set_text("Skill Issue");
+}
+
 // Called at the early stages of game loading
 MOD_EXTERN_FUNC void setup(CModInfo *info) noexcept {
   *info = modInfo.to_c();
@@ -26,11 +51,12 @@ MOD_EXTERN_FUNC void setup(CModInfo *info) noexcept {
   PaperLogger.info("Completed setup!");
 }
 
-// Called later on in the game loading - a good time to install function hooks
-MOD_EXTERN_FUNC void late_load() noexcept {
-  il2cpp_functions::Init();
+MOD_EXTERN_FUNC void late_load() {
+    il2cpp_functions::Init();
 
-  PaperLogger.info("Installing hooks...");
+    PaperLogger.info("Installing hooks...");
 
-  PaperLogger.info("Installed all hooks!");
+    INSTALL_HOOK(PaperLogger, LevelUIHook);
+
+    PaperLogger.info("Installed all hooks!");
 }
